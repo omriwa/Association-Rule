27d648424480dc4aaaa745b4a62d216ec530f3d6
omri wallach <omriwallach1992@gmail.com>
2021-01-14 15:19:17 +0100

    building transactions data structure

--- Apriori/main.py
+++ Apriori/main.py
@@ -6,37 +6,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Market_Basket_Optimisation.csv')
-X = dataset.iloc[:, 2:-1].values
+dataset = pd.read_csv('Market_Basket_Optimisation.csv',header=None)
+transactions = []
 
-# Using the elbow method to find the optimal number of clusters
-from sklearn.cluster import KMeans as model
+for i in range(0, len(dataset)):
+    transactions.append([])
+    for j in range(0, len(dataset.iloc[i])):
+        if(pd.notna(dataset.iloc[i][j])):
+            transactions[i].append(dataset.iloc[i][j])
 
-wcss = []
+# Training the model
 
-for i in range(1, 11):
-    kmeans = model(n_clusters=i,random_state=42)
-    kmeans.fit(X)
-    wcss.append(kmeans.inertia_)
 
-plt.plot(range(1, 11), wcss)
-plt.title("The Elbow Method")
-plt.show()
 
-# using the optimal number of clusters
-kmeans = model(n_clusters=5, random_state=42)
-y_pred = kmeans.fit_predict(X)
 
 # Visualizing the prediction
-import random
 
-plt.scatter(X[y_pred == 0, 0], X[y_pred == 0, 1], s=100, c="red", label="C1")
-plt.scatter(X[y_pred == 1,0], X[y_pred == 1,1],s=100, c="blue" , label="C2")
-plt.scatter(X[y_pred == 2,0], X[y_pred == 2,1],s=100, c="green" , label="C3")
-plt.scatter(X[y_pred == 3,0], X[y_pred == 3,1],s=100, c="yellow" , label="C4")
-plt.scatter(X[y_pred == 4, 0], X[y_pred == 4, 1], s=100, c="purple", label="C5")
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c="black", label="Centroids")
-plt.legend()
-plt.show()
 
 
